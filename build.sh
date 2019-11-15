d744efdce3f1fdc259f5f4f1cf8b5364e7a794cb
9dogs <hellishbot@gmail.com>
2019-11-15 16:35:33 +0300

    Install fpm and dependencies in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -eux
 
+yum update -y
+yum install -y \
+	wget \
+	bzip2 \
+	make \
+	gcc \
+	gettext \
+	libicu-devel \
+	rpm-build \
+	ruby-devel
+
+# Install FPM
+gem install --no-ri --no-rdoc fpm 
 # Download and extract MyStem
 curl -L https://download.cdn.yandex.net/mystem/mystem-3.1-linux-64bit.tar.gz | tar xvz -C .
 
